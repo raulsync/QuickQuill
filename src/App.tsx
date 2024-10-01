91ecdea7985d7123ef7b3bf6ff20bb63a1ae5898
raulsync <rahul.aanand086@gmail.com>
2024-10-01 13:21:43 +0530

    code_refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-// import { useState } from 'react'
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home';
 import Quill from './pages/Quill';
+import NavBar from './components/NavBar';
+import ViewQuill from './pages/ViewQuill';
+
+// Define a layout component to include the NavBar and an Outlet
+const Layout = () => (
+  <div>
+    <NavBar />
+    <Outlet />
+  </div>
+);
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/quill',
-    element: <Quill />,
-  },
-  {
-    path: '/quill/:id',
-    element: <div className="text-center font-bold">Hello test</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/quill',
+        element: <Quill />,
+      },
+      {
+        path: '/quill/:id',
+        element: <ViewQuill />,
+      },
+    ],
   },
 ]);
+
 function App() {
   return <RouterProvider router={router} />;
 }
