646ecce36835f9f7aa0736fda3561309a1baed0a
raulsync <rahul.aanand086@gmail.com>
2024-10-01 13:22:12 +0530

    navbar_homepage_ui

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import { NavLink } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <div className="flex flex-row justify-evenly mt-8">
+      <NavLink
+        to={'/'}
+        className={'text-blue-500 font-semibold'}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to={'/quill'}
+        className={'text-blue-500 font-semibold'}
+      >
+        All Quill
+      </NavLink>
+    </div>
+  );
+}
+
+export default NavBar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,59 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import { addToQuill, updateToQuill } from '../slices/quillSlice';
+
 function Home() {
-  return <div>Home</div>;
+  const [title, setTitle] = useState<string>('');
+  const [contentValue, setContentValue] = useState<string>('');
+  const [searchParam, setSearchParam] = useSearchParams();
+  const quillId = searchParam.get('quill');
+
+  const dispatch = useDispatch();
+
+  function createQuill() {
+    const quill = {
+      title: title,
+      content: contentValue,
+      id: quillId,
+    };
+
+    if (quillId) {
+      dispatch(updateToQuill(quill));
+    } else {
+      dispatch(addToQuill(quill));
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center gap-7">
+      <div className="flex justify-center mt-20 gap-5 w-full">
+        <input
+          className="px-5 w-[32%] py-2 rounded-lg bg-gray-800"
+          type="text"
+          placeholder="Enter your title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <button
+          onClick={createQuill}
+          className="bg-blue-500 px-4 rounded-lg font-semibold"
+        >
+          {quillId ? 'Update my quill' : 'Create my quill'}
+        </button>
+      </div>
+      <div className="w-full flex justify-center">
+        <textarea
+          className="p-5 min-w-[43%] bg-gray-800 rounded-lg"
+          rows={20}
+          placeholder="enter your content here"
+          value={contentValue}
+          onChange={(e) => setContentValue(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
